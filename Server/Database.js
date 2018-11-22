64e948ff10b9c5f6e54bf914a6043239f96e941c
DESKTOP-GPLG7RG\Benjamin <benjamin157@hotmail.fr>
2018-11-22 16:25:43 +0100

    function getUser

--- Server/Database.js
+++ Server/Database.js
@@ -13,3 +13,13 @@ con.connect(function(err){
     if (err)throw err;
     console.log("Connected to DB");
 });
+
+function getUser(login,pwd){
+
+    var Query = "SELECT * FROM user WHERE Login='"+login+"' and Password='"+pwd+"';";
+    con.query(Query,function(err,rows){
+        if(err) throw err;
+        return rows;
+    });
+
+}
